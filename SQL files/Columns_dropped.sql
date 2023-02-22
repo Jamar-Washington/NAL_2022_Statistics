be61f99abf0e10cd20be67f227be07c134e74e4d
Jamar-Washington <jamarxwashington@gmail.com>
2023-02-22 11:54:15 -0600

    Dropping columns that are not needed

---
+++ SQL files/Columns_dropped.sql
@@ -0,0 +1,58 @@
+-- Remove Field Goal % from Kicking table
+Alter Table al_kicker Drop Column fg_per;
+Alter Table ca_kicker Drop Column fg_per;
+Alter Table co_kicker Drop Column fg_per;
+Alter Table jx_kicker Drop Column fg_per;
+Alter Table or_kicker Drop Column fg_per;
+Alter Table sa_kicker Drop Column fg_per;
+
+-- Remove Completion, Interception, and Touchdown Pass % from Kicking table
+Alter Table al_passer Drop Column complete_per, Drop Column intercept_per, Drop Column tds_per;
+Alter Table ca_passer Drop Column complete_per, Drop Column intercept_per, Drop Column tds_per;
+Alter Table co_passer Drop Column complete_per, Drop Column intercept_per, Drop Column tds_per;
+Alter Table jx_passer Drop Column complete_per, Drop Column intercept_per, Drop Column tds_per;
+Alter Table or_passer Drop Column complete_per, Drop Column intercept_per, Drop Column tds_per;
+Alter Table sa_passer Drop Column complete_per, Drop Column intercept_per, Drop Column tds_per;
+
+-- Remove Punting data from Returning table
+Alter Table al_returner 
+Drop Column punt_return,
+Drop Column punt_yards,
+Drop Column avgpunt_yards,
+Drop Column punt_tds,
+Drop Column punt_long;
+
+Alter Table ca_returner 
+Drop Column punt_return,
+Drop Column punt_yards,
+Drop Column avgpunt_yards,
+Drop Column punt_tds,
+Drop Column punt_long;
+
+Alter Table co_returner 
+Drop Column punt_return,
+Drop Column punt_yards,
+Drop Column avgpunt_yards,
+Drop Column punt_tds,
+Drop Column punt_long;
+
+Alter Table jx_returner 
+Drop Column punt_return,
+Drop Column punt_yards,
+Drop Column avgpunt_yards,
+Drop Column punt_tds,
+Drop Column punt_long;
+
+Alter Table or_returner 
+Drop Column punt_return,
+Drop Column punt_yards,
+Drop Column avgpunt_yards,
+Drop Column punt_tds,
+Drop Column punt_long;
+
+Alter Table sa_returner 
+Drop Column punt_return,
+Drop Column punt_yards,
+Drop Column avgpunt_yards,
+Drop Column punt_tds,
+Drop Column punt_long;
